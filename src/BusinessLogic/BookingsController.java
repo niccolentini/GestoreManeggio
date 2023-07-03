dfbce33741d1e2edaa6fb9d83d1ddaa9af410d3f
giorgiamodi <giorgia.modi@stud.unifi.it>
2023-07-03 18:20:05 +0200

    Correzione addRiderToLesson

--- src/BusinessLogic/BookingsController.java
+++ src/BusinessLogic/BookingsController.java
@@ -1,6 +1,7 @@
 package BusinessLogic;
 
 import DAO.LessonDAO;
+import DAO.MembershipDAO;
 import DAO.RiderDAO;
 import DomainModel.*;
 
@@ -11,11 +12,13 @@ public class BookingsController implements Observer{
     private final LessonDAO lessonDAO;
     private final LessonsController lessonsController;
     private final RidersController ridersController;
+    private final MembershipDAO membershipDAO;
 
-    public BookingsController(LessonDAO lessonDAO, LessonsController lessonsController, RidersController ridersController) {
+    public BookingsController(LessonDAO lessonDAO, LessonsController lessonsController, RidersController ridersController, MembershipDAO membershipDAO) {
         this.lessonDAO = lessonDAO;
         this.lessonsController = lessonsController;
         this.ridersController = ridersController;
+        this.membershipDAO = membershipDAO;
     }
 
     public void addRiderToLesson (String riderFiscalCode, int lessonId) throws Exception {
@@ -24,8 +27,12 @@ public class BookingsController implements Observer{
         if (l == null) throw new RuntimeException("The given lesson id does not exist");
         if (r == null) throw new RuntimeException("The given rider does not exist");
 
-        int numLess = r.getMembership().getNumLessons();
+        getLessonsForRider(riderFiscalCode).forEach(lesson -> {
+            if (lesson.getDate().equals(l.getDate()) && lesson.getTime().equals(l.getTime()))
+                throw new RuntimeException("Il rider è già iscritto ad un'altra lezione in questo orario");
+        });
 
+        int numLess = r.getMembership().getNumLessons();
         if (numLess > 0){
             if ( l.getNumRider() == l.getMaxRiders())
                 throw new RuntimeException("Prenotazione non andata a buon fine. Sono state raggiunte le prenotazioni massime per questa lezione");
@@ -34,19 +41,10 @@ public class BookingsController implements Observer{
             if (riders.contains(r))
                 throw new RuntimeException("Il rider è già iscritto a questa lezione");
             r.getMembership().setNumLessons(numLess - 1);
+            membershipDAO.update(riderFiscalCode, r.getMembership());
             lessonDAO.addRiderToLesson(riderFiscalCode, lessonId);
         }
-
-
-        getLessonsForRider(riderFiscalCode).forEach(lesson -> {
-            if (lesson.getDate().equals(l.getDate()) && lesson.getTime().equals(l.getTime()))
-                throw new RuntimeException("Il rider è già iscritto ad un'altra lezione in questo orario");
-        });
-
-        //TODO: controllare se l'abbonamento è valido
-
-        lessonDAO.addRiderToLesson(riderFiscalCode, lessonId);
-
+        else throw new RuntimeException("Prenotazione non andata a buon fine. Il rider non ha più lezioni disponibili nel proprio pacchetto lezioni");
     }
 
     public void removeRiderFromLesson (String fiscalCode, int lessonId) throws Exception{
