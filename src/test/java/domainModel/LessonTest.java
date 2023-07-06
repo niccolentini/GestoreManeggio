4b10fab82d817adcbead7dd11301511d482bd983
niccolentini <niccolo.lentini@stud.unifi.it>
2023-07-06 22:49:49 +0200

    finished TEST

--- src/test/java/domainModel/LessonTest.java
+++
@@ -1,5 +0,0 @@
-package test.java.domainModel;
-
-public class LessonTest {
-
-}
--- src/test/java/domainModel/membership/BoxPackTest.java
+++ src/test/java/domainModel/membership/BoxPackTest.java
@@ -1,4 +1,23 @@
 package test.java.domainModel.membership;
+import main.java.DomainModel.Membership.BoxPack;
+import main.java.DomainModel.Membership.LessonsPack;
+import main.java.DomainModel.Membership.Membership;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class BoxPackTest {
+    @Test
+    public void LessonPack_test() throws Exception {
+        Membership m = new BoxPack();
+        Membership l = new LessonsPack(m);
+        Assertions.assertEquals(10, l.getNumLessons());
+    }
+    @Test
+    public void LessonPack_test2() throws Exception {
+        Membership m = new BoxPack();
+        Membership l = new LessonsPack(m);
+        Assertions.assertEquals(10, l.getNumLessons());
+        l.setNumLessons(5);
+        Assertions.assertEquals(5, l.getNumLessons());
+    }
 }
